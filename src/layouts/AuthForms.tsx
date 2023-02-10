4f4b5d738a52891314a99930bcafcb317430743c
mrx7355608 <mrx7355608@gmail.com>
2023-02-10 12:55:11 -0800

    added logo in AuthForms layout

--- src/layouts/AuthForms.tsx
+++ src/layouts/AuthForms.tsx
@@ -2,7 +2,13 @@ import { Outlet } from "react-router-dom";
 
 export default function AuthForms() {
     return (
-        <div className="flex items-center justify-space-between mw-100 mh-100">
+        <div className="font-serif flex flex-col items-center justify-center min-w-screen min-h-screen">
+            <div className="flex items-center justify-center gap-x-3 mb-12 mx-auto">
+                <img src="/logo.svg" alt="logo" className="w-4 h-auto" />
+                <h2 className="text-3xl font-bold text-gray-800">
+                    Social Media
+                </h2>
+            </div>
             <Outlet />
         </div>
     );
