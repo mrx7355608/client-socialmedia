0472d533cc07c7c5d864354f47f603ef8a2a5fdf
mrx7355608 <109037712+mrx7355608@users.noreply.github.com>
2023-02-10 14:19:26 +0000

    created 404 not found page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import AuthForms from "./layouts/AuthForms";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import NotFound from "./pages/NotFound";
 
 function App() {
     return (
@@ -16,6 +17,7 @@ function App() {
                     <Route path="login" element={<Login />} />
                     <Route path="signup" element={<Signup />} />
                 </Route>
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </Router>
     );
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,8 @@
+export default function NotFound(){
+    return (
+        <div className="flex flex-col items-center justify-center mw-100 mh-100">
+            <h1 className="font-4xl text-gray-800">404</h1>
+            <h4 className="font-2xl text-gray-400">Page not found</h4>
+        </div>
+    )
+}
