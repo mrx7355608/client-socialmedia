2685601880ae0957be2abb11241cdcd0191ec077
mrx7355608 <mrx7355608@gmail.com>
2023-02-10 15:12:50 -0800

    added profile page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // Layouts
 import AuthForms from "./layouts/AuthForms";
+import MainLayout from "./layouts/MainLayout";
 // Pages
 import Home from "@/pages/Home";
-import NotFound from "@/pages/NotFound";
 import Login from "@/pages/Auth/Login";
 import Signup from "@/pages/Auth/Signup";
-import MainLayout from "./layouts/MainLayout";
+import NotFound from "@/pages/NotFound";
+import Profile from "@/pages/Profile";
 
 function App() {
     return (
@@ -14,6 +15,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<MainLayout />}>
                     <Route index element={<Home />} />
+                    <Route path="profile" element={<Profile />} />
                 </Route>
                 <Route path="/auth" element={<AuthForms />}>
                     <Route path="login" element={<Login />} />
