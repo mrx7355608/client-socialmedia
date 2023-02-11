73d6764ac05fbc8cc992dcf62c8770057607519c
mrx7355608 <mrx7355608@gmail.com>
2023-02-11 11:39:44 -0800

    combined friends and photos button in profile div

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -4,7 +4,7 @@ export default function Profile() {
     return (
         <>
             {/* user image and some data */}
-            <div className="p-5 bg-white rounded-lg shadow-md">
+            <div className="p-5 bg-white rounded-lg shadow-md mb-5">
                 <img
                     src="/user.png"
                     className="rounded-full w-28 h-28 mx-auto mb-6"
@@ -15,14 +15,15 @@ export default function Profile() {
                 <p className="text-center text-gray-500 font-medium">
                     zerotwo@life.com
                 </p>
-            </div>
-            <div className="py-5">
-                <button className="rounded-full py-2 px-6 bg-gray-800 font-medium text-white border-2 border-gray-900">
-                    Photos
-                </button>
-                <button className="rounded-full py-2 px-6 bg-gray-800 font-medium text-white border-2 border-gray-900">
-                   Friends 
-                </button>
+                <hr className="my-5" />
+                <div>
+                    <button className="mr-9 bg-transparent font-medium text-gray-900">
+                        Photos
+                    </button>
+                    <button className="bg-transparent font-medium text-gray-900">
+                        Friends
+                    </button>
+                </div>
             </div>
 
             {/* Posts */}
