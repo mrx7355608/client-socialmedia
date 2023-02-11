fb52aebb9ef26a4fbf18b9606be31dca5bda96de
mrx7355608 <mrx7355608@gmail.com>
2023-02-11 11:39:08 -0800

    added icons for like and commet buttons

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,17 +1,29 @@
-export default function Post () {
+import { BiLike, BiComment } from "react-icons/bi";
+
+export default function Post() {
     return (
         <div className="flex flex-col p-4 rounded-lg shadow-md bg-white">
             {/* Author */}
             <div className="flex mb-3 gap-x-2 items-center">
-                <img src="/user.png" alt="author" className="w-12 h-12 rounded-full" />
+                <img
+                    src="/user.png"
+                    alt="author"
+                    className="w-12 h-12 rounded-full"
+                />
                 <div>
                     <p className="text-gray-800 font-medium">Zero Two 002</p>
-                    <p className="text-sm font-medium text-gray-500">Posted on Fri 10 Mar</p>
+                    <p className="text-sm font-medium text-gray-500">
+                        Posted on Fri 10 Mar
+                    </p>
                 </div>
             </div>
 
             {/* Post body */}
-            <p className="mb-7">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus cumque quod tempore cupiditate explicabo consequatur repellat fugit reprehenderit, perferendis assumenda.</p>
+            <p className="mb-7">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Temporibus cumque quod tempore cupiditate explicabo consequatur
+                repellat fugit reprehenderit, perferendis assumenda.
+            </p>
 
             {/* Total likes and comments on post */}
             <div className="text-gray-400 mb-2">
@@ -23,8 +35,28 @@ export default function Post () {
             {/* Like and Comment button */}
             <div className="flex gap-x-2 bg-transparent">
                 {/* TODO: use icons */}
-                <button className="flex-1 p-2 rounded-md bg-gray-200">Like</button>
-                <button className="flex-1 p-2 rounded-md bg-gray-200">Comment</button>
+                <button className="flex-1 p-2 pb-3 rounded-md bg-gray-200 font-medium">
+                    <BiLike
+                        size="20px"
+                        style={{
+                            display: "inline",
+                            color: "inherit",
+                            marginRight: "8px",
+                        }}
+                    />
+                    Like
+                </button>
+                <button className="flex-1 p-2 pb-3 rounded-md bg-gray-200 font-medium">
+                    <BiComment
+                        size="20px"
+                        style={{
+                            display: "inline",
+                            color: "inherit",
+                            marginRight: "8px",
+                        }}
+                    />
+                    Comment
+                </button>
             </div>
         </div>
     );
