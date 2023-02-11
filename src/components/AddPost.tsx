d88ad349555f39436ab37b848b67a36d5bccccde
mrx7355608 <mrx7355608@gmail.com>
2023-02-11 01:54:12 -0800

    replaced div with img

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -2,7 +2,7 @@ export default function AddPost() {
     return (
         <div className="p-3 rounded-lg shadow-md flex justify-center items-center bg-white gap-x-2">
             {/* User Image */}
-            <div className="rounded-full w-10 h-9 bg-black"></div>
+            <img src="/user.png" className="rounded-full w-9 h-9 bg-black" />
             {/* Post body */}
             <input
                 className="rounded-full w-full bg-gray-100 px-4 py-1.5"
