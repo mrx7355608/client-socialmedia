d02b44fc284aa9f1fe9949dc213e58c02ac1909d
mrx7355608 <mrx7355608@gmail.com>
2023-02-22 12:10:39 -0800

    created helper functions for dispatching

--- src/contexts/auth/reducer.ts
+++ src/contexts/auth/reducer.ts
@@ -1,16 +1,39 @@
-import { AuthActions, IAuthActionTypes } from "./actions";
-import { IAuthState } from "./state";
+import { AuthActions, IAuthActionTypes, IAuthError, IUserFetchedAuth } from "./actions";
+import { IAuthState, IUser } from "./state";
 
 export function authReducer(state: IAuthState, action: AuthActions): IAuthState {
     switch (action.type) {
         case IAuthActionTypes.Logout:
             return { ...state, user: null };
         case IAuthActionTypes.AuthError:
-            return { ...state, error: action.payload.error };
+            return { ...state, error: action.payload.error, isLoading: false };
         case IAuthActionTypes.UserFetched:
-            return { ...state, user: action.payload.user };
+            return { ...state, user: action.payload.user, isLoading: false };
 
         default:
             return state;
     }
+}
+
+// Dispatch functions
+export function logoutUser() {
+    return {
+        type: IAuthActionTypes.Logout,
+    };
+}
+
+export function userDataFetched(data: IUser): IUserFetchedAuth {
+    return {
+        type: IAuthActionTypes.UserFetched,
+        payload: { user: data },
+    };
+}
+
+export function apiDown(): IAuthError {
+    return {
+        type: IAuthActionTypes.AuthError,
+        payload: {
+            error: "It seems that server is down",
+        },
+    };
 }
