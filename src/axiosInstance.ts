def73e321c03c9af6470190d94970ede76faf55c
mrx7355608 <mrx7355608@gmail.com>
2023-02-22 21:07:20 -0800

    updated axios instance to include creds

--- src/axiosInstance.ts
+++ src/axiosInstance.ts
@@ -4,6 +4,7 @@ import { config } from "../config/config";
 const axiosInstance = axios.create({
     baseURL: config.apiUrl,
     timeout: 5000,
+    withCredentials: true,
 });
 
 export default axiosInstance;
