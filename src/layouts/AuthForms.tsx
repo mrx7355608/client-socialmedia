41916154fdf544951af1a94fc09f662e685be093
mrx7355608 <mrx7355608@gmail.com>
2023-02-23 02:52:55 -0800

    protect guest routes

--- src/layouts/AuthForms.tsx
+++ src/layouts/AuthForms.tsx
@@ -1,6 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { useAuth } from "../contexts/auth/context";
+import { Navigate, Outlet } from "react-router-dom";
+import Spinner from "@/components/Spinner";
 
 export default function AuthForms() {
+    const { state } = useAuth();
+
+    if (state.isLoading) {
+        return <Spinner />;
+    }
+    if (state.error) {
+        return (
+            <h3 className="font-bold text-red-800 my-auto text-center text-3xl">{state.error}</h3>
+        );
+    }
+
+    if (state.user) {
+        return <Navigate to="/" />;
+    }
+
     return (
         <div className="font-serif flex flex-col items-center justify-center min-w-screen min-h-screen">
             <div className="flex items-center justify-center gap-x-3 mb-8 mx-auto">
