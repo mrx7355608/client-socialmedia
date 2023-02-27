ea919067e5192c051b8d130877aec38009d40882
mrx7355608 <mrx7355608@gmail.com>
2023-02-27 07:44:18 -0800

    moved skeleton animations in a folder

---
+++ src/components/SkeletonAnimations/UserBoxAnimations.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function UserBoxAnimations() {
+    return (
+        <div className="bg-white border-0 my-3 shadow-lg rounded-lg p-4 max-w-sm w-full mx-auto">
+            <div className="animate-pulse flex-col">
+                {/* Profile picture circle */}
+                <div className="flex items-center gap-x-2 w-full">
+                    <div className="rounded-full bg-slate-400 h-10 w-10"></div>
+                    {/* Name and Posted at lines */}
+                    <div className="flex flex-col gap-y-2">
+                        <div className="h-2 bg-slate-400 rounded w-32"></div>
+                        <div className="h-2 bg-slate-400 rounded w-28"></div>
+                    </div>
+                </div>
+            </div>
+            {/* Add an action button here */}
+        </div>
+    );
+}
