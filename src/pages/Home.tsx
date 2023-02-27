cbe0c97a19fc51415cb99fc359849d7c086b0428
mrx7355608 <mrx7355608@gmail.com>
2023-02-27 10:52:20 -0800

    added infinite scroll

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,27 +2,67 @@ import AddPost from "@/components/AddPost";
 import Post from "@/components/Post";
 import PostSkeletonLoadingAnimation from "@/components/SkeletonAnimations/PostAnimation";
 import useTimeline from "@/hooks/useTimeline";
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { UserServices } from "@/services/user.services";
+import { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+export interface IPost {
+    author: {
+        authorId: string;
+        fullname: string;
+        profilePicture: string;
+        linkToProfile: string;
+    };
+    body: string;
+    createdAt: Date;
+    _id: string;
+}
 
 export default function Home() {
-    // Fetch user timeline
-    const { loading, error, timeline } = useTimeline();
+    const userServices = new UserServices();
+    const [page, setPage] = useState(1);
+    const [isMoreContent, setMoreContent] = useState(true);
+    const [timeline, setTimeline] = useState<IPost[]>([]);
+
+    useEffect(() => {
+        // Update url's page query value
+        const url = `/posts/me/timeline?page=${page}`;
+
+        // Fetch user timeline
+        (async () => {
+            const { data } = await userServices.getMyTimeline<IPost[]>(url);
+            if (data.length < 10) setMoreContent(false);
+            setTimeline([...timeline, ...data]);
+        })();
+    }, [page]);
 
     return (
         <div>
             <AddPost />
-            {loading ? (
-                <>
-                    <PostSkeletonLoadingAnimation />
-                    <PostSkeletonLoadingAnimation />
-                    <PostSkeletonLoadingAnimation />
-                </>
-            ) : (
-                timeline.map((post) => {
-                    return <Post key={(post as any)._id} postData={post} />;
-                })
-            )}
+            <InfiniteScroll
+                dataLength={timeline.length}
+                next={() => setPage(page + 1)}
+                hasMore={isMoreContent}
+                loader={<InfiniteScrollLoader />}
+                scrollThreshold={0.9}
+                endMessage={
+                    <p className="text-gray-400 text-center font-medium">No more posts to show</p>
+                }
+            >
+                {timeline.map((post) => {
+                    return <Post key={post._id} postData={post} />;
+                })}
+            </InfiniteScroll>
         </div>
+    );
+}
+
+function InfiniteScrollLoader() {
+    return (
+        <>
+            <PostSkeletonLoadingAnimation />
+            <PostSkeletonLoadingAnimation />
+            <PostSkeletonLoadingAnimation />
+        </>
     );
 }
