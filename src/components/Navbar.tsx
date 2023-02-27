cdcbd3f9a4a6acc23bb8ea225a17187bd00044b5
mrx7355608 <mrx7355608@gmail.com>
2023-02-27 07:44:33 -0800

    added comments

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,12 +15,14 @@ export default function Navbar() {
     return (
         <>
             <div className="flex items-center justify-between p-4 bg-white shadow-md gap-x-2">
+                {/* Logo */}
                 <div className="ml-1 flex items-center gap-x-2">
                     <img src="/logo.svg" alt="logo" className="w-3 h-auto" />
                     <h3 className="font-serif text-xl font-bold text-gray-800">
                         <Link to="/">Social Media</Link>
                     </h3>
                 </div>
+                {/* Search button */}
                 <div>
                     <button
                         onClick={openSearchModal}
@@ -30,20 +32,25 @@ export default function Navbar() {
                     </button>
                 </div>
             </div>
+            {/* Navigation links */}
+            {/* Home */}
             <div className="flex items-center justify-center gap-x-2 p-2">
                 <button className="flex-1 text-center bg-white py-3 rounded-lg shadow-md">
                     <Link to="/">
                         <RiHome7Line size="18px" style={{ margin: "0 auto" }} />
                     </Link>
                 </button>
+                {/* Profile */}
                 <button className="flex-1 bg-white py-3 rounded-lg shadow-md">
                     <Link to="/profile">
                         <CgProfile size="18px" style={{ margin: "0 auto" }} />
                     </Link>
                 </button>
+                {/* Pending requests */}
                 <button className="flex-1 bg-white py-3 rounded-lg shadow-md">
                     <HiOutlineUsers size="19px" style={{ margin: "0 auto" }} />
                 </button>
+                {/* Settings */}
                 <button className="flex-1 bg-white py-3 rounded-lg shadow-md">
                     <Link to="/settings">
                         <IoSettingsOutline size="19px" style={{ margin: "0 auto" }} />
