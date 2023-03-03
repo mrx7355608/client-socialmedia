436f445269c4307be19cd8a4d97db148b4b4f96c
mrx7355608 <mrx7355608@gmail.com>
2023-03-03 12:29:39 -0800

    refactored homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,33 +20,37 @@ export default function Home() {
         // Fetch user timeline
         (async () => {
             const { success, data, error } = await userServices.getMyTimeline(url);
-            if (error) setError(error);
+            if (error) return setError(error);
             if (success) {
                 if (data.length < 10) setMoreContent(false);
                 return setTimeline([...timeline, ...data]);
             }
-            setMoreContent(false);
         })();
     }, [page]);
 
     return (
         <div>
             <AddPost />
-            <h3 className="text-2xl font-medium text-red-600 text-center">{err ? err : null}</h3>
-            <InfiniteScroll
-                dataLength={timeline.length}
-                next={() => setPage(page + 1)}
-                hasMore={isMoreContent}
-                loader={<InfiniteScrollLoader />}
-                scrollThreshold={0.9}
-                endMessage={
-                    <p className="text-gray-400 text-center font-medium">No more posts to show</p>
-                }
-            >
-                {timeline.map((post) => {
-                    return <Post key={post._id} data={post} />;
-                })}
-            </InfiniteScroll>
+            {err ? (
+                <h3 className="text-2xl font-medium text-red-600 text-center mt-5">{err}</h3>
+            ) : (
+                <InfiniteScroll
+                    dataLength={timeline.length}
+                    next={() => setPage(page + 1)}
+                    hasMore={isMoreContent}
+                    loader={<InfiniteScrollLoader />}
+                    scrollThreshold={0.9}
+                    endMessage={
+                        <p className="text-gray-400 text-center font-medium">
+                            No more posts to show
+                        </p>
+                    }
+                >
+                    {timeline.map((post) => {
+                        return <Post key={post._id} data={post} />;
+                    })}
+                </InfiniteScroll>
+            )}
         </div>
     );
 }
