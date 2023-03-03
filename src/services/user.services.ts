93f631dcb02d650f2f75a43457450c0527997d3c
mrx7355608 <mrx7355608@gmail.com>
2023-03-03 13:03:27 -0800

    applied serviceHandler to all services

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -4,98 +4,47 @@ import { IUser } from "@/contexts/auth/state";
 import serviceHandler from "./serviceHandler";
 
 export class UserServices {
-    // TODO: create an error handling function for axios errors
-    // TODO: fix "ResponseType"
-
-    // TODO: fix data param type
-    private sendResponse<T>(success: boolean, data: T | null, error: string | null = null) {
-        return {
-            success,
-            data,
-            error,
-        };
-    }
-
-    private handleError(err: any): string {
-        if (err.response) {
-            return err.response.data.error;
-        } else if (err.request) {
-            return "It seems that server is down";
-        } else {
-            return "An un-expected error occured";
-        }
-    }
-
     getMyTimeline = serviceHandler(async (url: string) => {
         const response = await axiosInstance.get<IPost[]>(url);
         return response;
     });
 
-    async createPost(postData: { body: string }) {
-        try {
-            const response = await axiosInstance.post<IPost>("/posts", postData);
-            return this.sendResponse(true, response.data);
-        } catch (err: any) {
-            return this.sendResponse(false, null, err.message);
-        }
-    }
+    createPost = serviceHandler(async (postData: { body: string }) => {
+        const response = await axiosInstance.post<IPost>("/posts", postData);
+        return response;
+    });
 
-    async search(url: string) {
-        try {
-            const response = await axiosInstance.get<IUser[]>(url);
-            return this.sendResponse(true, response.data);
-        } catch (err: any) {
-            return this.sendResponse<null>(false, null, err.message);
-        }
-    }
+    search = serviceHandler(async (url: string) => {
+        const response = await axiosInstance.get(url);
+        return response;
+    });
 
-    async getMyFriends() {
-        try {
-            const response = await axiosInstance.get("/users/me/friends");
-            return this.sendResponse(true, response.data);
-        } catch (err: any) {
-            return this.sendResponse(false, null);
-        }
-    }
+    getMyFriends = serviceHandler(async () => {
+        const response = await axiosInstance.get("/users/me/friends");
+        return response;
+    });
 
-    async getMe() {
-        try {
-            const response = await axiosInstance.get("/users/me");
-            return this.sendResponse(true, response.data);
-        } catch (err: any) {
-            const errorMessage = this.handleError(err);
-            return this.sendResponse(false, errorMessage);
-        }
-    }
+    getMe = serviceHandler(async () => {
+        const response = await axiosInstance.get("/users/me");
+        return response;
+    });
 
-    async getApiHealth() {
-        try {
-            await axiosInstance.get("/health-check");
-            return this.sendResponse(true, null);
-        } catch (err) {
-            return this.sendResponse(false, null);
-        }
-    }
+    getApiHealth = serviceHandler(async () => {
+        const response = await axiosInstance.get("/health-check");
+        return response;
+    });
 
-    async getOneUser(id: string) {
-        try {
-            const response = await axiosInstance.get(`/users/${id}`);
-            return this.sendResponse(true, response.data);
-        } catch (err) {
-            return this.sendResponse(false, null);
-        }
-    }
+    getOneUser = serviceHandler(async (id: string) => {
+        const response = await axiosInstance.get(`/users/${id}`);
+        return response;
+    });
 
-    async updatePicture(file: any) {
-        try {
-            const response = await axiosInstance.patch("/users/me/change-picture", file, {
-                headers: {
-                    "Content-Type": "multipart/form-data",
-                },
-            });
-            return this.sendResponse(true, response.data);
-        } catch (err: any) {
-            return this.sendResponse(false, err.message);
-        }
-    }
+    updatePicture = serviceHandler(async (file: any) => {
+        const response = await axiosInstance.patch("/users/me/change-picture", file, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            },
+        });
+        return response;
+    });
 }
