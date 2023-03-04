ea173a9e55733e3672da4e09b091b99f993f675e
mrx7355608 <mrx7355608@gmail.com>
2023-03-03 19:27:53 -0800

    created a hook to handle timline stuff

--- src/hooks/useTimeline.tsx
+++ src/hooks/useTimeline.tsx
@@ -1,22 +1,23 @@
 import { UserServices } from "@/services/user.services";
-import React, { useEffect, useState } from "react";
+import { IPost } from "interfaces/post.interface";
+import { useEffect, useState } from "react";
 
-export default function useTimeline() {
-    const [loading, setLoading] = useState<boolean>(true);
-    const [timeline, setTimeline] = useState<[]>([]);
-    const [error, setError] = useState<string>("");
+export default function useTimeline(url: string) {
+    const [err, setError] = useState<string>("");
+    const [posts, setPosts] = useState<IPost[]>([]);
+    const [isMore, setIsMore] = useState<boolean>(true);
     const userServices = new UserServices();
 
     useEffect(() => {
-        (async function () {
-            const { success, data } = await userServices.getMyTimeline();
-            setLoading(false);
-            if (!success) {
-                return setError(data as string);
+        (async () => {
+            const { success, data, error } = await userServices.getMyTimeline(url);
+            if (error) return setError(error);
+            if (success) {
+                if (data.length < 10) setIsMore(false);
+                return setPosts([...posts, ...data]);
             }
-            return setTimeline(data as any);
         })();
-    }, []);
+    }, [url]);
 
-    return { loading, timeline, error };
+    return { err, posts, isMore };
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,69 +2,25 @@ import AddPost from "@/components/Posts/AddPost";
 import Post from "@/components/Posts/Post";
 import PostSkeletonLoadingAnimation from "@/components/SkeletonAnimations/PostAnimation";
 import { UserServices } from "@/services/user.services";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
-import { IPost } from "interfaces/post.interface";
-
-interface ITimeline {
-    loading: boolean;
-    err: string | null;
-    posts: IPost[];
-}
-interface IPaginationData {
-    isMore: boolean;
-    page: number;
-}
+import useTimeline from "@/hooks/useTimeline";
 
 export default function Home() {
-    const userServices = new UserServices();
-    const [timeline, setTimeline] = useState<ITimeline>({
-        loading: true,
-        err: null,
-        posts: [],
-    });
-    const [paginationData, setPaginationData] = useState<IPaginationData>({
-        isMore: true,
-        page: 1,
-    });
-
-    useEffect(() => {
-        const { page } = paginationData;
-        const { posts } = timeline;
-
-        // Update url's page query value
-        const url = `/posts/me/timeline?page=${page}`;
-
-        // Fetch user timeline
-        (async () => {
-            const { success, data, error } = await userServices.getMyTimeline(url);
-            if (error) return setTimeline({ ...timeline, err: error, loading: false });
-            if (success) {
-                if (data.length < 10) setPaginationData({ ...paginationData, isMore: false });
-                return setTimeline({
-                    ...timeline,
-                    loading: false,
-                    posts: [...posts, ...data],
-                });
-            }
-        })();
-    }, [paginationData.page]);
+    // Todo: add `no internet connection` error handling
+    const [page, setPage] = useState<number>(1);
+    const { err, posts, isMore } = useTimeline(`/posts/me/timeline?page=${page}`);
 
     return (
         <div>
             <AddPost />
-            {timeline.err ? (
-                <h3 className="text-2xl font-medium text-red-600 text-center mt-5">
-                    {timeline.err}
-                </h3>
+            {err ? (
+                <h3 className="text-2xl font-medium text-red-600 text-center mt-5">{err}</h3>
             ) : (
                 <InfiniteScroll
-                    dataLength={timeline.posts.length}
-                    next={() => {
-                        const newPage = paginationData.page + 1;
-                        setPaginationData({ ...paginationData, page: newPage });
-                    }}
-                    hasMore={paginationData.isMore}
+                    dataLength={posts.length}
+                    next={() => setPage(page + 1)}
+                    hasMore={isMore}
                     loader={<InfiniteScrollLoader />}
                     scrollThreshold={0.9}
                     endMessage={
@@ -73,7 +29,7 @@ export default function Home() {
                         </p>
                     }
                 >
-                    {timeline.posts.map((post) => {
+                    {posts.map((post) => {
                         return <Post key={post._id} data={post} />;
                     })}
                 </InfiniteScroll>
