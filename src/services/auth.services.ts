2192f2fc4d3a50c8f9b5ef9620890eab55266e62
mrx7355608 <mrx7355608@gmail.com>
2023-03-03 21:22:22 -0800

    wrapped service functions in serviceHandler

--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -1,57 +1,23 @@
-import { IUser } from "@/contexts/auth/state";
 import axiosInstance from "../axiosInstance";
+import serviceHandler from "./serviceHandler";
 
 interface ILoginData {
     email: string;
     password: string;
 }
-interface Response {
-    success: boolean;
-    message: string | null;
-    user: IUser | null;
-}
 
-// TODO: error handling in axios
 export class AuthServices {
-    private sendResponse(
-        success: boolean,
-        message: string | null,
-        user: IUser | null = null
-    ): Response {
-        return {
-            success,
-            message,
-            user,
-        };
-    }
+    login = serviceHandler(async (loginData: ILoginData) => {
+        const response = await axiosInstance.post("/auth/login", loginData, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        return response;
+    });
 
-    async login(loginData: ILoginData): Promise<Response> {
-        try {
-            const response = await axiosInstance.post("/auth/login", loginData, {
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            });
-            return this.sendResponse(true, null, response.data);
-        } catch (err: any) {
-            if (err.name === "TypeError") {
-                return this.sendResponse(false, "No internet connection");
-            }
-            return this.sendResponse(false, err.message);
-        }
-    }
-    async signup() {}
-    async loginAsGuest() {}
-    async logout() {
-        try {
-            const response = await axiosInstance.post("/auth/logout");
-            console.log(response.data);
-            if (response.status === 200 && response.data.logout === true) {
-                return this.sendResponse(true, null);
-            }
-            return this.sendResponse(false, null);
-        } catch (err: any) {
-            return this.sendResponse(false, "Something went wrong");
-        }
-    }
+    logout = serviceHandler(async () => {
+        const response = await axiosInstance.post("/auth/logout");
+        return response;
+    });
 }
--- src/services/post.services.ts
+++ src/services/post.services.ts
@@ -1,38 +1,20 @@
 import axiosInstance from "../axiosInstance";
+import serviceHandler from "./serviceHandler";
 
 export class PostServices {
-    private sendResponse<T>(success: boolean, data: T) {
-        return {
-            success,
-            data,
-        };
-    }
+    likePost = serviceHandler(async (id: string) => {
+        const url = `/posts/like/${id}`;
+        const response = await axiosInstance.patch(url);
+        return response;
+    });
 
-    async likePost(id: string) {
-        try {
-            const url = `/posts/like/${id}`;
-            const response = await axiosInstance.patch(url);
-            return this.sendResponse(true, response.data);
-        } catch (err: any) {
-            return this.sendResponse<string>(true, err.message);
-        }
-    }
+    createNewPost = serviceHandler(async (postBody: { body: string }) => {
+        const response = await axiosInstance.post("/posts", postBody);
+        return response;
+    });
 
-    async createNewPost(postBody: { body: string }) {
-        try {
-            const response = await axiosInstance.post("/posts", postBody);
-            return this.sendResponse(true, response.data);
-        } catch (err: any) {
-            return this.sendResponse<string>(true, err.message);
-        }
-    }
-
-    async getComments(postId: string) {
-        try {
-            const response = await axiosInstance.get(`/posts/comments/${postId}`);
-            return this.sendResponse(true, response.data);
-        } catch (err: any) {
-            return this.sendResponse<string>(true, err.message);
-        }
-    }
+    getComments = serviceHandler(async (postId: string) => {
+        const response = await axiosInstance.get(`/posts/comments/${postId}`);
+        return response;
+    });
 }
