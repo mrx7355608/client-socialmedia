84be1e451cc6dd9db949117e8574f97cf15e45b6
mrx7355608 <mrx7355608@gmail.com>
2023-03-07 21:18:47 -0800

    fixed component function name

--- src/components/SkeletonAnimations/PostAnimation.tsx
+++ src/components/SkeletonAnimations/PostAnimation.tsx
@@ -1,4 +1,4 @@
-export default function SkeletonLoadingAnimation() {
+export default function PostAnimation() {
     return (
         <div className="bg-white border-0 my-3 shadow-lg rounded-lg p-4 max-w-sm w-full mx-auto">
             <div className="animate-pulse flex-col">
