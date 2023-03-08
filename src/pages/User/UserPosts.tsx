20f11e08df6850a9d310594b39c3f951521953f6
mrx7355608 <mrx7355608@gmail.com>
2023-03-07 21:50:57 -0800

    fetching user posts

--- src/pages/User/UserPosts.tsx
+++ src/pages/User/UserPosts.tsx
@@ -1,5 +1,21 @@
-import React from "react";
+import useFetch from "@/hooks/useFetch";
+import { IPost } from "interfaces/post.interface";
+import Post from "../../components/Posts/Post";
 
 export default function UserPosts() {
-    return <div>UserPosts</div>;
+    const { loading, data: posts, err } = useFetch<IPost[]>("/posts/me", []);
+
+    return (
+        <div className="flex flex-col gap-y-4">
+            {loading ? (
+                <h3 className="font-medium">Loading...</h3>
+            ) : (
+                posts.map((post) => {
+                    return <Post data={post} key={post._id} />;
+                })
+            )}
+
+            {err ? <h3 className="text-red-600 font-medium text-lg">{err}</h3> : null}
+        </div>
+    );
 }
