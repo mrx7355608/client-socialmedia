4c9c5219d5348f3d794b33c4bfb52446e100a964
mrx7355608 <mrx7355608@gmail.com>
2023-03-07 21:51:31 -0800

    refactored - using useFetch custom hook

--- src/pages/User/UserFriends.tsx
+++ src/pages/User/UserFriends.tsx
@@ -1,52 +1,24 @@
 import Spinner from "@/components/Spinner";
-import { UserServices } from "@/services/user.services";
-import React, { useEffect, useState } from "react";
-
-interface IUserFriend {
-    _id: string;
-    firstname: string;
-    lastname: string;
-    profilePicture: string;
-}
-
-interface IFriendsState {
-    loading: boolean;
-    err: string | null;
-    friends: IUserFriend[];
-}
+import useFetch from "@/hooks/useFetch";
+import { IFriend } from "interfaces/friends.interface";
 
 export default function UserFriends() {
-    const userServices = new UserServices();
-    const [userFriendsData, setData] = useState<IFriendsState>({
-        loading: true,
-        err: null,
-        friends: [],
-    });
+    const { loading, data: friends, err } = useFetch<IFriend[]>("/users/me/friends", []);
 
-    useEffect(function () {
-        (async function () {
-            const { success, data, error } = await userServices.getMyFriends();
-            if (error) return setData({ ...userFriendsData, err: error });
-            if (success) {
-                return setData({ ...userFriendsData, friends: data.friends, loading: false });
-            }
-        })();
-    }, []);
-
-    if (userFriendsData.loading) return <Spinner />;
+    if (loading) return <Spinner />;
 
     return (
         <div>
-            {userFriendsData.err ? (
-                <h3 className="text-xl font-medium text-red-600 mt-5">{userFriendsData.err}</h3>
+            {err ? (
+                <h3 className="text-xl font-medium text-red-600 mt-5">{err}</h3>
             ) : (
-                userFriendsData.friends.map((frnd) => <Friend key={frnd._id} friend={frnd} />)
+                friends.map((frnd) => <Friend key={frnd._id} friend={frnd} />)
             )}
         </div>
     );
 }
 
-function Friend({ friend }: { friend: IUserFriend }) {
+function Friend({ friend }: { friend: IFriend }) {
     return (
         <div className="p-3 rounded-lg bg-white shadow-sm">
             <p className="text-xl font-medium text-purple-700">
