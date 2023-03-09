f7dfca95975122b7e8bb00fe7be8b1bced88befa
mrx7355608 <mrx7355608@gmail.com>
2023-03-08 20:43:31 -0800

    changed icon

--- src/components/Toasts/ErrorToast.tsx
+++ src/components/Toasts/ErrorToast.tsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
-import { BsFillCheckCircleFill } from "react-icons/bs";
+import { VscError } from "react-icons/vsc";
 
 export default function ErrorToast({ message }: { message: string }) {
     return (
         <div className="transition delay-100 bottom-4 left-4 flex justify-between items-center w-max p-3 px-4 rounded-lg bg-red-200 text-red-800 font-medium shadow-lg fixed text-sm">
             <div className="flex gap-x-2 items-center">
-                <BsFillCheckCircleFill />
+                <VscError />
                 <p className="pb-0.5">{message}</p>
             </div>
             <p className="pb-0.5 ml-9">x</p>
