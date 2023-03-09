a0c33c375ade59218d5533ff430fb0cf471edc1e
mrx7355608 <mrx7355608@gmail.com>
2023-03-08 21:52:10 -0800

    fixed the error response returned from this hook

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,5 +1,6 @@
 import axiosInstance from "../axiosInstance";
 import React, { useEffect, useState } from "react";
+import { AxiosError } from "axios";
 
 export default function useFetch<T>(url: string, initialData: T) {
     const [loading, setLoading] = useState<boolean>(true);
@@ -13,9 +14,15 @@ export default function useFetch<T>(url: string, initialData: T) {
                 setLoading(false);
                 setData(resp.data);
             })
-            .catch((error) => {
+            .catch((error: AxiosError) => {
                 setLoading(false);
-                setError(error);
+                let errorMessage = "Something went wrong!";
+                if (error.response) {
+                    errorMessage = (error.response.data as any).error;
+                } else if (error.request) {
+                    errorMessage = "It seems that the server is down";
+                }
+                return setError(errorMessage);
             });
     }, []);
 
