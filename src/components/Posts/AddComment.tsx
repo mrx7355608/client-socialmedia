21c635b311fb78deada0d4cc4c1a913b689e57f4
mrx7355608 <mrx7355608@gmail.com>
2023-03-10 03:17:20 -0800

    adding a loading indicator

--- src/components/Posts/AddComment.tsx
+++ src/components/Posts/AddComment.tsx
@@ -2,7 +2,7 @@ import { useAuth } from "@/contexts/auth/context";
 import { usePost } from "@/contexts/post/context";
 import { useToast } from "@/contexts/toast/context";
 import { PostServices } from "@/services/post.services";
-import React, { Dispatch, SetStateAction, useRef, useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import { IComment } from "./Comment";
 
 export default function AddComment({
@@ -12,8 +12,9 @@ export default function AddComment({
 }) {
     const postServices = new PostServices();
     const [comment, setComment] = useState<string>("");
+    const [loading, setLoading] = useState<boolean>(false);
     const { state } = useAuth();
-    const { post } = usePost();
+    const { post, setPost } = usePost();
     const { showSuccessToast, showWarningToast, showErrorToast } = useToast();
 
     return (
@@ -26,24 +27,45 @@ export default function AddComment({
                 onChange={(e) => setComment(e.target.value)}
                 value={comment}
             />
-            <button
-                onClick={async () => await commentOnPost()}
-                className="rounded-md p-1 px-3 pb-1.5 bg-gray-900 text-white text-sm font-medium"
-            >
-                Post
-            </button>
+            {!loading ? (
+                <button
+                    onClick={async () => await commentOnPost()}
+                    className="rounded-md p-1 px-3 pb-1.5 bg-gray-900 text-white text-sm font-medium"
+                >
+                    Post
+                </button>
+            ) : (
+                <button className="rounded-md p-1 px-3 pb-1.5 bg-gray-900 text-white text-sm font-medium">
+                    Posting...
+                </button>
+            )}
         </div>
     );
 
     async function commentOnPost() {
-        if (!comment) return showWarningToast("Comment cannot be empty");
+        // Show error if comment is empty
+        if (!comment.trim()) {
+            return showWarningToast("Comment cannot be empty");
+        }
+
+        setLoading(true);
+
+        // Make a request to server
         const { success, data, error } = await postServices.commentOnPost({
             comment,
             postId: post._id,
         });
+
+        // Change loading state
+        setLoading(false);
+
         if (error) return showErrorToast(error);
         if (success) {
             setComments((prev) => [data, ...prev]);
+            setPost({
+                ...post,
+                comments: [data, ...post.comments],
+            });
             setComment("");
             return showSuccessToast("Comment added successfully!");
         }
