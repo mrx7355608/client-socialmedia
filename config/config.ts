a070f0014d28c8e695de81432de8d367bedc7851
mrx7355608 <mrx7355608@gmail.com>
2023-03-13 09:58:20 -0700

    added guest login creds in config file

--- config/config.ts
+++ config/config.ts
@@ -1,3 +1,7 @@
 export const config = {
     apiUrl: import.meta.env.VITE_API_URL,
+
+    // Guest account credentials
+    guestEmail: import.meta.env.VITE_GUEST_ACCOUNT_EMAIL,
+    guestPassword: import.meta.env.VITE_GUEST_ACCOUNT_PASSWORD,
 };
