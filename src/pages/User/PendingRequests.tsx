6de72a9919f93191d13514107db33146a99fb571
mrx7355608 <mrx7355608@gmail.com>
2023-03-13 10:01:44 -0700

    added loading states for accept/reject request

--- src/pages/User/PendingRequests.tsx
+++ src/pages/User/PendingRequests.tsx
@@ -1,7 +1,9 @@
 import { useAuth } from "@/contexts/auth/context";
-import { usePost } from "@/contexts/post/context";
 import { useToast } from "@/contexts/toast/context";
+import { useState } from "react";
 import { UserServices } from "@/services/user.services";
+import { requestAccepted, requestRejected } from "@/contexts/auth/reducer";
+import { IPendingRequest } from "@/contexts/auth/state";
 
 export default function PendingRequests() {
     const { state } = useAuth()
@@ -10,10 +12,18 @@ export default function PendingRequests() {
             <h2 className="font-serif font-bold text-2xl my-5 text-gray-800">
                 Pending Requests
             </h2>
-            <div className="flex flex-col shadow-md">
+            <div className="flex flex-col">
                 {
+                    state.user?.pendingRequests.length < 1 ? 
+                    <h3 className="font-medium text-xl text-gray-700 text-center">No pending requests</h3> 
+                    : 
                     state.user?.pendingRequests.map(req => {
-                            return <Request key={req.friendId} userId={req.friendId} userName={req.firstname + req.lastname} userImage={req.profilePicture} />
+                            return <Request 
+                                key={req.friendId} 
+                                userId={req.friendId} 
+                                userName={req.firstname + req.lastname} 
+                                userImage={req.profilePicture} 
+                            />
                         })
                 }
             </div>
@@ -24,6 +34,8 @@ export default function PendingRequests() {
 
 function Request({ userImage, userName, userId }: { userImage: string; userName: string, userId: string }) {
     const userServices = new UserServices()
+    const [acceptLoading, setAccpetLoading] = useState<boolean>(false);
+    const [rejectLoading, setRejectLoading] = useState<boolean>(false);
     const { state, dispatch } = useAuth()
     const { showErrorToast, showSuccessToast } = useToast()
 
@@ -34,22 +46,34 @@ function Request({ userImage, userName, userId }: { userImage: string; userName:
                 <p className="font-medium text-gray-800 text-lg">{userName}</p>
             </div>
             <div className="flex w-full mt-6 gap-x-3">
-                <button onClick={acceptRequest} className="text-sm flex-1 p-2 px-3 bg-gray-100 rounded-md pb-2.5 text-gray-700 font-medium">
-                    Accept
-                </button>
-                <button onClick={rejectRequest} className="text-sm flex-1 p-2 px-3 bg-gray-100 rounded-md pb-2.5 text-red-600 font-medium">
-                    Reject 
-                </button>
+                { acceptLoading ?
+                    <button className="text-sm flex-1 p-2 px-3 bg-gray-100 rounded-md pb-2.5 text-gray-700 font-medium">
+                        Accepting...
+                    </button>
+                    :
+                    <button onClick={acceptRequest} className="text-sm flex-1 p-2 px-3 bg-gray-100 rounded-md pb-2.5 text-gray-700 font-medium">
+                        Accept
+                    </button>
+                }
+                { rejectLoading ?
+                    <button className="text-sm flex-1 p-2 px-3 bg-gray-100 rounded-md pb-2.5 text-red-600 font-medium">
+                        Rejecting... 
+                    </button>
+                    : 
+                    <button onClick={rejectRequest} className="text-sm flex-1 p-2 px-3 bg-gray-100 rounded-md pb-2.5 text-red-600 font-medium">
+                        Reject 
+                    </button>
+                }
             </div>
         </div>
     );
 
-    // TODO: add loading states
+    // Reject request
     async function rejectRequest () {
         const { success, error, data } = await userServices.rejectRequest(userId)
-        console.log(data)
+        const newPendingRequests = state.user?.pendingRequests.filter(p => p.friendId !== userId);
         if (success) {
-            // dispatch(requestRejected())
+            dispatch(requestRejected(newPendingRequests as IPendingRequest[]))
             return showSuccessToast("Request rejected!")
         }
 
@@ -57,11 +81,12 @@ function Request({ userImage, userName, userId }: { userImage: string; userName:
         return showErrorToast("An un-expected error occured")
      }
 
+    // Accept request
     async function acceptRequest () {
         const { success, error, data } = await userServices.acceptRequest(userId)
-        console.log(data)
+        const newPendingRequests = state.user?.pendingRequests.filter(p => p.friendId !== userId);
         if (success) {
-            // dispatch(requestAccepted())
+            dispatch(requestAccepted(newPendingRequests as IPendingRequest[]))
             return showSuccessToast("Request accepted!")
         }
 
