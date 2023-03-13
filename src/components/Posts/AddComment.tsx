6907df68b5344e92c1d286ba838d61d353d3cc81
mrx7355608 <mrx7355608@gmail.com>
2023-03-13 09:53:37 -0700

    added border-top

--- src/components/Posts/AddComment.tsx
+++ src/components/Posts/AddComment.tsx
@@ -18,7 +18,7 @@ export default function AddComment({
     const { showSuccessToast, showWarningToast, showErrorToast } = useToast();
 
     return (
-        <div className="absolute bottom-0 bg-white w-full flex items-center justify-center gap-x-2 mt-2 p-3">
+        <div className="border-t-2 border-gray-200 absolute bottom-0 bg-white w-full flex items-center justify-center gap-x-2 mt-2 p-3">
             <img className="w-8 h-8 rounded-full" src={state.user?.profilePicture} />
             <input
                 className="rounded-full w-full p-2 px-3  bg-gray-200 text-sm"
